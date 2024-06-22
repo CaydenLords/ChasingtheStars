8e2edcbb566736b0b45c192022d8b4574df078fd
CaydenLords <lor20002@byui.edu>
2024-06-21 18:21:32 -0600

    Wahoo

---
+++ testBody.h
@@ -0,0 +1,29 @@
+#pragma once
+
+/***********************************************************************
+ * Header File:
+ *    Test Position : Test the Body class
+ * Author:
+ *    Cayden Lords
+ * Summary:
+ *    All the unit tests for Body
+ ************************************************************************/
+
+
+#pragma once
+
+#include "position.h"
+#include "unitTest.h"
+
+ /*******************************
+  * TEST Position
+  * A friend class for Position which contains the Position unit tests
+  ********************************/
+class TestBody : public UnitTest
+{
+public:
+   void run()
+   {
+
+   }
+};
